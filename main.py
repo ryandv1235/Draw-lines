64fdb2829d0b2b04f15a730e4ae3a567daf1013f
Ryan <devogelryan@gmail.com>
2020-06-27 12:36:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+draw = turtle.Turtle()
+draw.shape('circle')
+
+draw.forward(100)
+draw.right(90)
+draw.forward(100)
+draw.right(90)
+draw.forward(100)
+draw.right(90)
+draw.forward(100)
+
+
