5b9edd4cd53e3b7dabc493c30f1eede3d151dbb1
Ryan <devogelryan@gmail.com>
2020-06-27 14:07:23 +0200

    new changes

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import turtle
 draw = turtle.Turtle()
 draw.shape('circle')
 
-draw.forward(100)
-draw.right(90)
-draw.forward(100)
-draw.right(90)
-draw.forward(100)
-draw.right(90)
-draw.forward(100)
+for x in range(4):
+	draw.forward(100)
+	draw.right(90)
+	draw.forward(100)
+	draw.right(90)
+	draw.forward(100)
+	draw.right(90)
+	draw.forward(100)
+
 
 
